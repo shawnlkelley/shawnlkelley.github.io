cc8abe427c6bd3db3c73b056e604c007e620ec90
mokera101 <shawn_kelley@msn.com>
2020-02-21 00:40:19 -0800

    added drop down
    added drop down list

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+
+
+// function for creating a selectable list
+function createList(listArray) {
+  // creates list element
+  var dropDown = document.createElement("select");
+
+
+  // adds list items to the list
+  for (index = 0; index < listArray.length; index++) {
+    var listItem = document.createElement("option");
+    listItem.setAttribute("value",listArray[index]);
+    listItem.append(listArray[index]);
+    dropDown.append(listItem);
+  }
+// returns completed drop down list
+  return dropDown;
+}
+
 // this creates a html p element and then adds a string to it
 var footerContent = document.createElement("p");
 footerContent.append("The current time is: ", Date());
@@ -9,9 +28,20 @@ document.getElementById("footer").appendChild(footerContent);
 var mainContent = document.createElement("p");
 mainContent.append("this is where main content goes");
 
+
+// creates a drop down with a list of domains
+let domainList = ["domain1.com", "domain2.com", "domain3.com"];
+
+var domainText = document.createElement("p");
+domainText.append("Choose a domain: ");
+domainText.append(createList(domainList));
+
+
+//mainContent.append(domainDrop);
+
 // this disiplays the previously created element on the page
 document.getElementById("app").appendChild(mainContent);
-
+document.getElementById("app").appendChild(domainText);
 // this creates a html p element and then adds a string to it
 var outputContent = document.createElement("p");
 outputContent.append("this is where output content goes");

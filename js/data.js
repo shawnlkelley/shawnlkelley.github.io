4bd32c0ac496f733d455e2f81b709c210f19f63d
mokera101 <shawn_kelley@msn.com>
2020-02-21 23:39:36 -0800

    add content
    add content

--- js/data.js
+++ js/data.js
@@ -9,12 +9,18 @@ const data = {
   boxType: 'list',
   contentId: 'domain-list',
   contentText: 'Domain: ',
-  listItems: ["domain1.com", "domain2.com", "domain3.com"]
+  listItems: ["domain.com", "sub1.domain.com", "sub2.domain.com"]
 },
 "deviceObj": {
   boxType: 'list',
   contentId: 'device-list',
   contentText: 'Device: ',
-  listItems: ["Device1", "Device2", "Device3"]
+  listItems: ["Device1.domain.com", "Device2.sub1.domain.com", "Device3.sub.domain.com"]
+},
+"ipAddress": {
+  boxType: 'textbox',
+  contentId: 'IP address',
+  contentText: 'IP address: ',
+  listItems: [null]
 },
 };
